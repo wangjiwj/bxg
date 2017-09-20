6fa4b2bf50e22013d499c1f284a26fce4d8f8bfd
wj <wj@qq.com>
2017-09-20 16:45:48 +0800

    模板引擎重构登录

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'], function ($) {
+define(['jquery','template','cookie'], function ($,template) {
 	//NProgress.start();
 	//NProgress.done();
 
@@ -36,4 +36,9 @@ define(['jquery','cookie'], function ($) {
 	//设置用户的头像信息
 	//$('.aside .profile img').attr('src',loginInfo.tc_avatar);
 	//$('.aside .profile h4').html(loginInfo.tc_name);
+	var tpl='<div class="avatar img-circle"> <img src="{{tc_avatar}}"></div>' +
+		'<h4>{{tc_name}}</h4>';
+	var html=template.render(tpl,loginInfo);
+	$('.aside .profile').html(html);
+
 });
