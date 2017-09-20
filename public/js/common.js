ecfe753b11c5f322dca9f22ba3b57bab438d5981
wj <wj@qq.com>
2017-09-20 11:54:00 +0800

    模块化重构

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,39 @@
-
-	NProgress.start();
-
-	NProgress.done();
+define(['jquery','cookie'], function ($) {
+	//NProgress.start();
+	//NProgress.done();
 
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+
+	$('#logoutBtn').click(function () {
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success: function (data) {
+				//console.log(222);
+				if(data.code==200){
+					//重新跳转到登录页
+					location.href='/main/login';
+				}
+			}
+		});
+	});
+
+	//检测用户是否登录
+	var flag=$.cookie('PHPSESSID');
+	console.log(flag);
+	if(!flag && location.pathname != '/main/login'){
+		//如果cookie不存在，跳转登录页
+		location.href='/main/login';
+	}
+
+	//设置用户头像信息
+	var loginInfo=$.cookie('loginInfo');
+	loginInfo=loginInfo && JSON.parse(loginInfo);
+	//设置用户的头像信息
+	//$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+	//$('.aside .profile h4').html(loginInfo.tc_name);
+});
