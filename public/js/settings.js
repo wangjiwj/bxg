fa512c1a318a4b4a054d5f820b4b8cf14c8d495b
wj <wj@qq.com>
2017-09-25 11:52:30 +0800

    添加富文本功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','uploadify','region'],function($,template){
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
     //调用接口获取个人信息
     $.ajax({
         type:'get',
@@ -29,6 +29,13 @@ define(['jquery','template','uploadify','region'],function($,template){
                 url:'/public/assets/jquery-region/region.json'
             });
 
+            //处理富文本
+            CKEDITOR.replace('editor',{
+                toolbarGroups:[
+                    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                    { name: 'colors', groups: [ 'colors' ] },
+                ]
+            })
         }
     })
 });
